61907f92758258174293ef92805927b2b0310eb5
Dzi4dk <louis.delhomme@utt.fr>
2023-11-13 08:59:26 +0100

    validé

--- TP_IntroGit/engine.js
+++ TP_IntroGit/engine.js
@@ -1,7 +1,7 @@
 var Engine = {
     check: function(){
-    console.log("Engine Ready and Clemence is ... !")
-    console.log("Blue Level checked".blue)
+    console.log("Engine Ready and Clemence is gay !")
+    console.log("Norme Loulou54#{8521}bf52 est validée avec validation".green)
     return true
     }
     }
