f5c31b6aee5d3375b5dc98d13ad52fe7d74fb0ec
clenemce <105370561+clenemce@users.noreply.github.com>
2023-12-07 01:00:25 +0100

    methodes (questions.js)

--- questions.js
+++ questions.js
@@ -109,3 +109,76 @@ module.exports = {
     dansLeTest,
     questionselectionnee
 };
+
+
+//METHODES
+
+class Question {
+    static afficher(questions, index) {
+        if (index >= 0 && index < questions.length) {
+            const question = questions[index];
+            console.log(`Titre : ${question.title}`);
+            console.log(`Format : ${question.format}`);
+            console.log(`Texte : ${question.text}`);
+            console.log(`Réponses : ${question.answers.join(', ')}`);
+            console.log(`Type de question : ${question.typeofQuestion()}`);
+        } else {
+            console.log('Index de question invalide.');
+        }
+    }
+
+    afficherliste(){
+
+    }
+}
+
+class QCM {
+    afficherQCM() {
+        this.questions.forEach((question, index) => {
+            console.log(`Question ${index + 1}:`);
+            Question.afficher(this.questions, index);
+            console.log();
+        });
+    }
+    
+    // Vérifiez si la question à l'index spécifié est déjà dans la liste questionselectionnee
+    questionDejaSelectionnee(index) {
+        return this.questionselectionnee.some(question => question.title === this.questions[index].title);
+    }
+
+    selectionnerQuestion(index) {
+        if (index >= 0 && index < this.questions.length) {
+            if (!this.questionDejaSelectionnee(index)) {
+                const selectedQuestion = new Question(
+                    this.questions[index].title,
+                    this.questions[index].format,
+                    this.questions[index].text,
+                    []
+                );
+                selectedQuestion.typeQuestion = this.questions[index].typeofQuestion();
+                this.questionselectionnee.push(selectedQuestion);
+                console.log(`Question ${index + 1} sélectionnée et ajoutée à la liste.`);
+            } else {
+                console.log(`La question à l'index ${index + 1} est déjà sélectionnée dans le test.`);
+            }
+        } else {
+            console.log('Index invalide. Veuillez spécifier un index valide.');
+        }
+    }
+
+    deselectionnerQuestion(index) {
+        if (!this.questionDejaSelectionnee(index)) {
+            console.log(`La question à l'index ${index + 1} n'est pas présente dans la liste.`);
+            return null;
+        } else if (index >= 0 && index < this.questionselectionnee.length) {
+            const deselectedQuestion = this.questionselectionnee.splice(index, 1)[0];
+            console.log(`Question ${index + 1} désélectionnée et retirée de la liste.`);
+            return deselectedQuestion;
+        } else {
+            console.log('Index invalide. Veuillez spécifier un index valide.');
+            return null;
+        }
+    }
+
+}
+
