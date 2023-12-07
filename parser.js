eaa279cda39a4e8464d923a704ea858a0a6eedb9
vignybrayan <vignybrayan01@gmail.com>
2023-12-07 07:38:37 +0100

    version 3.0

--- parser.js
+++ parser.js
@@ -1,6 +1,6 @@
 const { Question } = require('./qcm.js');
 const colors = require('colors');
-
+var _ = require('underscore');
 // Définition de la classe parser à utiliser
 class Parser {
     parsedQuestions = [];
@@ -160,37 +160,75 @@ class Parser {
         if (action === 'selection') {
             // Ajouter la question à la liste des questions sélectionnées
             // Vous pourriez avoir une propriété pour stocker les questions sélectionnées dans votre classe
-            this.questionsSelectionnees.push(this.parsedQuestions[numeroQuestion - 1]);
+            this.questionsSelectionnees.push(this.parsedQuestions[numeroQuestion - 0]);
             console.log(`Question ${numeroQuestion} sélectionnée.`.green);
         
-        
+            console.log(`${this.parsedQuestions[numeroQuestion].text} (${this.parsedQuestions[numeroQuestion].typeQuestion})`.green);
         
         } else if (action === 'deselection') {
             // Retirer la question de la liste des questions sélectionnées
-                    if (!this.questionsSelectionnees.includes(this.parsedQuestions[numeroQuestion - 1])) {
+                    if (!this.questionsSelectionnees.includes(this.parsedQuestions[numeroQuestion - 0])) {
                         console.log("On ne peut supprimer un element qui n'existe pas".red);
-                    } else if(this.questionsSelectionnees.includes(this.parsedQuestions[numeroQuestion - 1])){
+                    } else if(this.questionsSelectionnees.includes(this.parsedQuestions[numeroQuestion - 0])){
                         
                     // Vous pourriez avoir une propriété pour stocker les questions sélectionnées dans votre classe
-                    this.questionsSelectionnees = this.questionsSelectionnees.filter((_, index) => index + 1 !== numeroQuestion);
+                    this.questionsSelectionnees = this.questionsSelectionnees.filter((question) => question !== this.parsedQuestions[numeroQuestion]);
                     console.log(`Question ${numeroQuestion} désélectionnée.`.green);
                             }
         } else {
             console.log('Action invalide. Utilisez "selection" ou "deselection".'.red);
         }
 
-        // Afficher les questions sélectionnées
-        //let index = this.parsedQuestions.indexOf(this.questionsSelectionnees);
-        console.log(`${this.parsedQuestions[numeroQuestion].text} (${this.parsedQuestions[numeroQuestion].typeQuestion})`.green);
-        //console.log('Questions sélectionnées :', this.questionsSelectionnees.text);
+        
         
         
     }
-    ///
+    AfficherSelection(){
+        console.log(this.questionsSelectionnees.forEach((questions,index) =>{console.log(`Question ${index}: ${questions.text}   (${questions.typeQuestion})\n`.green);}));
+    }
+
+    hasDuplicates() {
+        return _.uniq(this.questionsSelectionnees).length !== this.questionsSelectionnees.length;
+    }
+
+    simulateTest() {
+        // Afficher le message de démarrage de la simulation
+        console.log('\n\tSimulation du test en cours...\n');
+        console.log('Le test est en marche commencer deja a remplir:\n');
+        let point=0;
+        // Boucler sur les questions sélectionnées
+        for (let i = 0; i < this.questionsSelectionnees.length; i++) {
+            const question = this.questionsSelectionnees[i];
+            
+            // Afficher la question
+            //console.log(``);
+    
+            // Demander à l'enseignant de fournir une réponse simulée
+            let userInput = readlineSync.question(`Question ${i + 1}: ${question.text} ________`);
+            let reponse=parseInt(userInput);
+            
+            const rep=`${question.answers}`;
+           // const regex = /\{=(.*?)\}/;
+            const match = rep.match(/\{=(.*?)\}/);
+            const chaineExtraite = match ? match[1] : null;
+            //console.log(chaineExtraite);
+            if(reponse===chaineExtraite){
+                point=point+1;
+            }
+    
+            // Afficher la réponse correcte (à des fins de vérification)
+            console.log(`Reponse correcte: ${question.answers}`);
+    
+            }
+    
+        // Afficher le message de fin de simulation
+        console.log('Simulation du test terminée.Et vous avez eu '+point+'/'+this.questionsSelectionnees.length);
+    }
 }
 
 let fs = require('fs');
 const readlineSync = require('readline-sync'); 
+let parser = new Parser();
 fs.readFile('U5-p49-Subject_verb_agreement.gift', 'utf8', function (err, data) {
     if (err) {
         return console.log(err);
@@ -204,8 +242,10 @@ fs.readFile('U5-p49-Subject_verb_agreement.gift', 'utf8', function (err, data) {
     //console.log(parser.questionsSelectionnees);
 
     while (true) {
-        let choix = readlineSync.keyInSelect(['Afficher tous les questions', 'Selectionner les questions du test','Afficher tous les questions', 'Quitter'], 'Que souhaitez-vous faire ?');
-        let parser = new Parser();
+        let choix = readlineSync.keyInSelect(['Afficher tous les questions', 'Selectionner les questions du test','Afficher tous les questions selectionnes','Qualiter du test','Simuler Test','Quitter'], 'Que souhaitez-vous faire ?');
+        
+        parser.parse(data);
+        
         if (choix === 0) {
                 
                 if (parser.errorCount === 0) {
@@ -213,20 +253,20 @@ fs.readFile('U5-p49-Subject_verb_agreement.gift', 'utf8', function (err, data) {
                     console.log("The .gift file contains error".red);
                 }
                 //console.log(parser.tokenize(data));
-                parser.parse(data);
-                console.log(parser.parsedQuestions.forEach((question,index) =>{console.log(`Question ${index}: ${question.text}   (${question.typeQuestion})\n`.green);}));
+                
+                console.log(parser.parsedQuestions.forEach((questions,index) =>{console.log(`Question ${index}: ${questions.text}   (${questions.typeQuestion})\n`.green);}));
                
           
         } else if (choix === 1) {
 
                     while (true) {
-                        let choix = readlineSync.keyInSelect(['selection', 'deselection', 'Terminer la selection'], 'Quel operation souhaitez vous utiliser ?');
+                        let choix1 = readlineSync.keyInSelect(['selection', 'deselection', 'Terminer la selection'], 'Quel operation souhaitez vous utiliser ?');
                     
-                        if (choix === 0) {
+                        if (choix1 === 0) {
                                     let userInput = readlineSync.question('entrer un entier ');
                                     let number=parseInt(userInput);
                                     
-                                    parser.parse(data);// en gros il faut rappeller le parser sinon parse.parsedQuestions=0
+                                    //parser.parse(data);// en gros il faut rappeller le parser sinon parse.parsedQuestions=0
                                     //verifier que le nombre entrer appartient à la liste de classe
                                     if (!isNaN(userInput) && userInput >= 0 && userInput < parser.parsedQuestions.length) {
                                         parser.QuestionSelection(number,"selection");
@@ -235,11 +275,11 @@ fs.readFile('U5-p49-Subject_verb_agreement.gift', 'utf8', function (err, data) {
                                         console.log('L\'index entré n\'appartient pas à la liste d\'instances de la classe question.'.red);
                                     }
 
-                        } else if (choix === 1) {
+                        } else if (choix1 === 1) {
                                     let userInput = readlineSync.question('entrer un entier ');
                                     let number=parseInt(userInput);
                                     
-                                    parser.parse(data);// en gros il faut rappeller le parser sinon parse.parsedQuestions=0
+                                    //parser.parse(data);// en gros il faut rappeller le parser sinon parse.parsedQuestions=0
                                     //verifier que le nombre entrer appartient à la liste de classe
                                     if ( !isNaN(userInput) && userInput >= 0 && userInput < parser.parsedQuestions.length) {
                                         parser.QuestionSelection(number,"deselection");
@@ -247,22 +287,30 @@ fs.readFile('U5-p49-Subject_verb_agreement.gift', 'utf8', function (err, data) {
                                     } else {
                                         console.log('L\'index entré n\'appartient pas à la liste d\'instances de la classe question.'.red);
                                     }
+                                    
+                                    
                                 
-                        } else if (choix === 2) {
-                        break; // Quitter le programme
+                        } else if (choix1 === 2) {
+                               break; // Quitter le programme
                         }
                     }
           
         } else if (choix === 2) {
-            
-                if(parser.questionsSelectionnees.length != 0){
-                console.log(parser.questionsSelectionnees.forEach((question,index) =>{console.log(`Question ${index}: ${question.text}   (${question.typeQuestion})\n`.green);}));
-                }else{
-                    console.log("Aucune question n a ete selectionne pour le test. Allez remplir les questions pour votre test".red);
-                }
-                
+              parser.AfficherSelection();
         
         }else if (choix === 3) {
+                        const a=parser.hasDuplicates();
+                    if(!a){
+                        console.log("Le test sans problème".green);
+                    }else{
+                        console.log("Le test contient des doublons de meme question".red);
+                    }
+              
+      }else if (choix === 4) {
+        parser.simulateTest();
+             
+      
+      }else if (choix === 5) {
             break; // Quitter le programme
           }
       }
